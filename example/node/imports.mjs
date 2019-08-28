cf2ba7a702e537865e5e5aa182ccbe87350819ef
Ingvar Stepanyan <me@rreverser.com>
2019-08-28 12:00:55 +0100

    Improve readability of Node example

--- example/node/imports.mjs
+++ example/node/imports.mjs
@@ -1,5 +1,4 @@
 import { createInterface } from 'readline';
-import { equal as assertEqual } from 'assert';
 
 const rl = createInterface({
   input: process.stdin,
@@ -7,18 +6,21 @@ const rl = createInterface({
 });
 rl.pause();
 
+function question(prompt) {
+  return new Promise(resolve => rl.question(prompt, resolve));
+}
+
 let counter = 0;
 
-export function read_number() {
-  return new Promise(resolve => {
-    rl.question(`Enter number #${++counter}: `, res => {
-      rl.pause();
+export async function read_number() {
+  let res = await question(`Enter number #${++counter}: `);
+  rl.pause();
 
-      let resNumber = res | 0;
-      assertEqual(resNumber, res);
-      resolve(resNumber);
-    });
-  });
+  let resNumber = res | 0;
+  if (resNumber != res) {
+    throw new Error('Invalid number.');
+  }
+  return resNumber;
 }
 
 export function print_number(num) {
--- example/node/index.mjs
+++ example/node/index.mjs
@@ -3,7 +3,9 @@ import { fileURLToPath } from 'url';
 import { Instance } from '../../asyncify.mjs';
 import * as testImports from './imports.mjs';
 
-const wasmContents = readFileSync(fileURLToPath(`${import.meta.url}/../../example.wasm`));
+const wasmContents = readFileSync(
+  fileURLToPath(`${import.meta.url}/../../example.wasm`)
+);
 const wasmModule = new WebAssembly.Module(wasmContents);
 const wasmInstance = new Instance(wasmModule, {
   env: testImports
