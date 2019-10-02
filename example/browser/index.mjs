5dab0e7c06d8d779d86e046432e2cf6511ef2366
Ingvar Stepanyan <rreverser@google.com>
2019-10-02 14:59:37 +0100

    Fix URL in browser example

--- example/browser/index.mjs
+++ example/browser/index.mjs
@@ -17,7 +17,7 @@
 import { instantiateStreaming } from '../../asyncify.mjs';
 import * as testImports from './imports.mjs';
 
-instantiateStreaming(fetch('../test.wasm'), {
+instantiateStreaming(fetch('../example.wasm'), {
   env: testImports
 })
   .then(result => result.instance.exports.run())
